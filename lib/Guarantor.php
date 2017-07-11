bb136a5fab599c8e86a76f759206bf773b33ea93
Allan Jesse Odeke <Allan Jesse Odeke>
2017-07-11 19:00:11 +0300

    Updates to the loan application, approval and dashboard

--- lib/Guarantor.php
+++ lib/Guarantor.php
@@ -30,14 +30,14 @@ class Guarantor extends Db {
 	public function getLoanGuarantors($filter = ""){
 		$fields = array( "`members`.`id`", "`phone`", "`shares`", "COALESCE((`deposit_sum`-`withdraws_sum`),0) `savings`","COALESCE((`loanAmount`-`amountPaid`),0) `outstanding_loan`", "`memberNames`" );
 		
-		$table = self::$table_name." JOIN ".(self::$members_sql)." ON `members`.`id` = ".(self::$table_name).".`memberId` LEFT JOIN ".(self::$deposits_sql)." ON `deposits`.`memberId` = ".(self::$table_name).".`memberId` LEFT JOIN ".(self::$withdraws_sql)." ON `withdraws`.`memberId` = ".(self::$table_name).".`memberId` LEFT JOIN ".(self::$shares_sql)." ON ".(self::$table_name).".`memberId` = `client_shares`.`memberId` LEFT JOIN ".(self::$loan_balances_sql)." ON ".(self::$table_name).".`memberId` = `loan_balances`.`memberId`";
+		$table = self::$table_name." JOIN ".(self::$members_sql)." ON `members`.`id` = ".(self::$table_name).".`memberId` JOIN ".(self::$deposits_sql)." ON `deposits`.`memberId` = ".(self::$table_name).".`memberId` LEFT JOIN ".(self::$withdraws_sql)." ON `withdraws`.`memberId` = ".(self::$table_name).".`memberId` JOIN ".(self::$shares_sql)." ON ".(self::$table_name).".`memberId` = `client_shares`.`memberId` LEFT JOIN ".(self::$loan_balances_sql)." ON ".(self::$table_name).".`memberId` = `loan_balances`.`memberId`";
 		
 		$where = $table = "";
 		if(is_numeric($filter)){
 			$where = "`loanAccountId`=".$filter;
 		}
 		else{
-			$table = (self::$members_sql)." LEFT JOIN ".(self::$deposits_sql)." ON `members`.`id` = `deposits`.`memberId` LEFT JOIN ".(self::$withdraws_sql)." ON `members`.`id` = `deposits`.`memberId` LEFT JOIN ".(self::$shares_sql)." ON `members`.`id` = `client_shares`.`memberId` LEFT JOIN ".(self::$loan_balances_sql)." ON `members`.`id` = `loan_balances`.`memberId`";
+			$table = (self::$members_sql)." LEFT JOIN ".(self::$deposits_sql)." ON `members`.`id` = `deposits`.`memberId` JOIN ".(self::$withdraws_sql)." ON `members`.`id` = `deposits`.`memberId` JOIN ".(self::$shares_sql)." ON `members`.`id` = `client_shares`.`memberId` LEFT JOIN ".(self::$loan_balances_sql)." ON `members`.`id` = `loan_balances`.`memberId`";
 			$where = $filter;
 		}
 		
