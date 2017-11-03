8e57005004486f01a896ae59aac77438dc13606d
Allan Odeke <ajoluvya@gmail.com>
2017-11-03 16:02:32 +0300

    Bug fixes

--- server_processing.php
+++ server_processing.php
@@ -139,7 +139,7 @@ if ( isset($_POST['page']) && $_POST['page'] == "deposit_accounts" ) {
 	
 	$primary_key = "`deposit_account`.`id`";
 
-	$columns = array( "`deposit_account`.`id`", "`clientNames`", "`clientType`", "`clientId`", "`productName`", "`deposit_account`.`maxWithdrawalAmount`", "`deposit_account`.`recomDepositAmount`", "`sumWithdrawn`", "`sumDeposited`", "CONCAT('dep_',`deposit_account`.`id`) `DT_RowId`", "`deposit_account`.`dateCreated`" );
+	$columns = array( "`deposit_account`.`id`", "`clientNames`", "`clientType`", "`clientId`", "`productName`", "`deposit_account`.`maxWithdrawalAmount`", "`deposit_account`.`recomDepositAmount`", "COALESCE(`sumWithdrawn`,0) `sumWithdrawn`", "(COALESCE(`sumDeposited`,0)+`openingBalance`) `sumDeposited`", "CONCAT('dep_',`deposit_account`.`id`) `DT_RowId`", "`deposit_account`.`dateCreated`" );
 
 }
 if ( isset($_POST['page']) && strlen($_POST['page'])>0) {
